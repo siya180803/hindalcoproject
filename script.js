b724424cf3e18f3e4c14473c5cff7fd0885f0e6b
Nikunj Agarwal <nikunjagarwal0711@gmail.com>
2025-05-18 21:59:53 +0530

    API issue fixed

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-console.log("hello");
 document.addEventListener("DOMContentLoaded", function () {
-  const url = "https://sheetdb.io/api/v1/zkfigzvgn8d0a";
+  const url = "https://mocki.io/v1/8a6e6561-74a1-4b1d-821b-1542e72d72f0"; // Replace with your actual mock API URL
 
   let data = [];
 
@@ -14,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function displayCards(dataToDisplay) {
     const cardGrid = document.getElementById("card-grid");
-    cardGrid.innerHTML = ""; // Clear previous cards
+    cardGrid.innerHTML = "";
 
     dataToDisplay.forEach((item) => {
       if (item?.breakdown?.length > 0) {
@@ -22,15 +21,15 @@ document.addEventListener("DOMContentLoaded", function () {
         card.className = "card";
 
         card.innerHTML = `
-                  <h3><strong>Breakdown:</strong> ${item.breakdown}</h3>
-                  <h3>Date: ${item.Name}</h3>
-                  <p><strong>SHIFT:</strong> ${item.SHIFT}</p>
-                  <p><strong>Area:</strong> ${item.Area}</p>
-                  <p><strong>Action taken:</strong> ${item["Action taken"]}</p>
-                  <p><strong>Pending jobs:</strong> ${item["pending jobs"]}</p>
-                  <p><strong>Spare consumed:</strong> ${item["spare consumed"]}</p>
-                  <p><strong>Time taken:</strong> ${item["time taken"]}</p>
-              `;
+          <h3><strong>Breakdown:</strong> ${item.breakdown}</h3>
+          <h3><strong>Date:</strong> ${item.Name}</h3>
+          <p><strong>SHIFT:</strong> ${item.SHIFT}</p>
+          <p><strong>Area:</strong> ${item.Area}</p>
+          <p><strong>Action taken:</strong> ${item["Action taken"]}</p>
+          <p><strong>Pending jobs:</strong> ${item["pending jobs"]}</p>
+          <p><strong>Spare consumed:</strong> ${item["spare consumed"]}</p>
+          <p><strong>Time taken:</strong> ${item["time taken"]}</p>
+        `;
 
         cardGrid.appendChild(card);
       }
@@ -40,14 +39,16 @@ document.addEventListener("DOMContentLoaded", function () {
   document
     .getElementById("search-button")
     .addEventListener("click", function (event) {
-      event.preventDefault(); // Prevent the default form submission
+      event.preventDefault();
 
       const searchText = document
         .getElementById("search-input")
         .value.toLowerCase();
+
       const filteredData = data.filter((item) =>
         item.breakdown.toLowerCase().includes(searchText)
       );
+
       displayCards(filteredData);
     });
 });
